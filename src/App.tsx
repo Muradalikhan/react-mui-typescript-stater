cc8c02c98cfe4668e7e3553565701cfbdc38a878
Murad khan <murad.jaddoon@gmail.com>
2022-11-10 14:17:48 +0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
+import Routes from './routes/Routes'
+import UserContainer from './context/UserContext'
+import { SnackbarMessage } from './components/core/SnackbarMessage'
+import { MessageContainer } from './context/MessageContext'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(): JSX.Element {
+    return (
+        <HelmetProvider>
+            <MessageContainer>
+                <BrowserRouter>
+                    <UserContainer>
+                        <Routes />
+                    </UserContainer>
+                </BrowserRouter>
+                <SnackbarMessage />
+            </MessageContainer>
+        </HelmetProvider>
+    )
 }
-
-export default App;
